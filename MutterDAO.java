498e38e776f3b40e0d594bf7be1c6a79de25b286
ken <ken@DESKTOP-MA9I00B>
2020-03-03 02:24:25 +0900

    13－6完成

--- MutterDAO.java
+++ MutterDAO.java
@@ -14,5 +14,46 @@ public class MutterDAO {
 
 	private final String JDBC_URL="jdbc:h2:tcp://localhost/~/docotsubu";
 	private final String DB_USER="sa";
-	private final String DB_PASS
+	private final String DB_PASS="";
+
+	public List<Mutter>findAll(){
+		List<Mutter> mutterList=new ArrayList<>();
+
+		try(Connection conn=DriveManager.getConnection(JDBC_URL,DB_USER,DB_PASS)){
+			String sql="SELECT ID,NAME,TEXT FROM MUTTER ORDER BY ID DESC";
+			PreparedStatement pStmt=conn.prepareStatement(sql);
+
+			ResultSet rs=pStmt=.executeQuery();
+
+			while(re.next()) {
+				int id=rs.getInt("ID");
+				String userName=rs.getString("NAME");
+				String text=rs.getString("TEXT");
+				Mutter mutter=new Mutter(id,userName,text);
+				mutterList.add(mutter);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return mutterList;
+	}
+	public boolean create(Mutter mutter) {
+		try(Connection conn=DriverManager.getConnection(JDBC_URL,DB_USER,DB_PASS)){
+
+			String sql="INSERT INFO MUTTER(NAME,TEXT) VALUES(?,?)";
+			PreparedStatement pStmt=conn.prepareStatement(sql);
+			pStmt.setString(1, mutter.getUserName());
+			pStmt.setString(2, mutter.getText());
+
+			int result=pStmt.executeUpdate();
+			if(result!=1) {
+				return false;
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+		}
 }
